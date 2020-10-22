f5d71ca23dc87c3e8f217f65e45041d9b79b6ce8
MrCodingRobot <davalosaeduardo@gmail.com>
2020-10-22 00:02:38 -0500

    adding ids to widgets

--- monopoly/gui/Lobby/lobby_classes.py
+++ monopoly/gui/Lobby/lobby_classes.py
@@ -6,16 +6,12 @@ class Lobby(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
+    #def update(self):
+    #    print(self.ids.items())
+
 
 class LobbyGrid(GridLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-    def print_ids(self):
-        for key, val in self.ids.items():
-            print("key={0}, val={1}".format(key, val))
 
-
-class PlayerInfo(GridLayout):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
