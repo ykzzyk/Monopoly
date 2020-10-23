0f345562b03aa4400d30bed19107db9788723fe2
ykzzyk <nz9024295@gmail.com>
2020-10-22 22:36:57 -0500

    buttons

--- monopoly/gui/Game/game_classes.py
+++ monopoly/gui/Game/game_classes.py
@@ -13,28 +13,23 @@ class Game(Screen):
 class GameBoard(GridLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.dice_dict = {1: '\u2680', 2: '\u2681', 3: '\u2682', 4: '\u2683', 5: '\u2684', 6: '\u2685'}
-        self.dice_1 = random.choice(list(self.dice_dict.values()))
-        self.dice_2 = random.choice(list(self.dice_dict.values()))
-        print(self.dice_1)
 
     def roll_dice(self, event):
+        dice_dict = {1: '\u2680', 2: '\u2681', 3: '\u2682', 4: '\u2683', 5: '\u2684', 6: '\u2685'}
+        dice_1 = random.choice(list(dice_dict.values()))
+        dice_2 = random.choice(list(dice_dict.values()))
 
+        self.ids.dice_1.text = f'[b][color=#000000]{dice_1}[/color][/b]'
+        self.ids.dice_2.text = f'[b][color=#000000]{dice_2}[/color][/b]'
 
-
-        '''
         step_1 = list(dice_dict.keys())[list(dice_dict.values()).index(dice_1)]
         step_2 = list(dice_dict.keys())[list(dice_dict.values()).index(dice_2)]
 
         steps = step_1 + step_2
         print(steps)
 
-        dice = Label(text='', font_size='20', pos=(500, 1000))
-
-        dice.text = f'{dice_1} {dice_2}'
-        print(dice.text)
-
+        """
         if step_1 == step_2:
             print('Please roll again!')
             self.roll_dice(event)
-        '''
+        """
