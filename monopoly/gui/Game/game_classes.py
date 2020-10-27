13aec240099a3129dfea8fd2f8c7c3cd7249bab4
ykzzyk <nz9024295@gmail.com>
2020-10-27 18:20:13 -0500

    popup window

--- monopoly/gui/Game/game_classes.py
+++ monopoly/gui/Game/game_classes.py
@@ -1,8 +1,12 @@
 from kivy.uix.screenmanager import Screen
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.widget import Widget
+from kivy.uix.popup import Popup
+from kivy.clock import Clock
 from kivy.core.window import Window
 from kivy.uix.label import Label
+import queue
 import random
 
 
@@ -13,6 +17,7 @@ class Game(Screen):
 class GameBoard(GridLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.cardInfo = CardInfoPop()
 
     def roll_dice(self, event):
         dice_dict = {1: '\u2680', 2: '\u2681', 3: '\u2682', 4: '\u2683', 5: '\u2684', 6: '\u2685'}
@@ -33,3 +38,56 @@ class GameBoard(GridLayout):
             print('Please roll again!')
             self.roll_dice(event)
         """
+
+    def cardInfoPopup(self):
+        self.cardInfo.get_card(self.cardInfo.chance, 'chance')
+        self.cardInfo.open()
+        Clock.schedule_once(lambda dt: self.cardInfo.dismiss(), 4)
+
+
+class CardInfoPop(Popup):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.chance = queue.Queue(maxsize=0)
+        self.chance.put('ADVANCE TO ST.CHARLES PLACE.\nIF YOU PASS GO, COLLECT $200.')
+        self.chance.put(
+            'MAKE GENERAL REPAIRS\nON ALL YOUR PROPERTY:\nFOR EACH HOUSE PAY $25.\nFOR EACH HOTEL, PAY $100.')
+        self.chance.put('GO BACK THREE SPACES.')
+        self.chance.put('GET OUT OF JAIL FREE.')
+        self.chance.put('ADVANCE TO BOARDWALK.')
+        self.chance.put('TAKE A TRIP TO READING\nRAILROAD. IF YOU PASS GO,\nCOLLECT $200.')
+        self.chance.put('YOUR BULIDING LOAN MATURES.\nCOLLECT $150.')
+        self.chance.put('ADVANCE TO ILLINOIS AVENUE.\nIF YOU PASS GO, COLLECT $200.')
+        self.chance.put('ADVANCE TO THE NEAREST\nUTILITY.')
+        self.chance.put('GO DIRECTLY TO JAIL.\nDO NOT COLLECT $200.')
+        self.chance.put('SPEEDING FINE. PAY $15.')
+        self.chance.put('ADVANCE TO THE NEXT\nRAILROAD.')
+        self.chance.put('YOU HAVE BEEN ELECTED\nCHAIRMAN OF THE BOARD.\nPAY EACH PLAYER $50.')
+
+        self.chest = queue.Queue(maxsize=0)
+        self.chest.put('LIFE INSURANCE MATURES.\nCOLLECT $100.')
+        self.chest.put('YOU INHERIT $100.')
+        self.chest.put('SCHOOL FEES. PAY $50.')
+        self.chest.put('GET OUT OF JAIL FREE.')
+        self.chest.put('INCOME TAX REFUND.\nCOLLECT $20.')
+        self.chest.put('HOLIDAY FUND MATURES.\nCOLLECT $100.')
+        self.chest.put("DOCTOR'S FEES. PAY $50.")
+        self.chest.put('HOSPITAL FEES. PAY $100.')
+        self.chest.put('GO DIRECTLY TO JAIL.\nDO NOT COLLECT $200.')
+        self.chest.put("COLLECT $25 CONSULTANCY FEE.\nIT'S YOUR BIRTHDAY.\nCOLLECT $10 FROM EACH PLAYER.")
+        self.chest.put('FROM SALE OF STOCK, YOU GET $50.')
+        self.chest.put('YOU HAVE WON SECOND PRIZE\nIN A BEAUTY CONTEST.\nCOLLECT $10.')
+        self.chest.put('YOU ARE ASSESSED FOR STREET REPAIRS:\nPAY $40 PER HOUSE AND $115 PER HOTEL YOU OWN.')
+        self.chest.put('ADVANCE TO GO. COLLECT $200.')
+        self.chest.put('BANK ERROR IN YOUR FAVOR.\nCOLLECT $200.')
+
+    def get_card(self, cards, name):
+        card = cards.get()
+        cards.put(card)
+
+        # Change the card texts
+        self.ids.card_info.text = f'[b][color=#000000]{card}[/color][/b]'
+        # Change the display image
+        self.ids.card_image.source = f"assets/{name}.png"
+
+        return card
