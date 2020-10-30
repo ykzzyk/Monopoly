d37931e590f63a4434fab4dcd78355d718efe523
ykzzyk <nz9024295@gmail.com>
2020-10-29 23:49:08 -0500

    button

--- monopoly/gui/Game/game_classes.py
+++ monopoly/gui/Game/game_classes.py
@@ -52,7 +52,7 @@ class GameBoard(Widget):
 
         # print(Window.width)
 
-        self.circle1 = Line(circle=(kivy.metrics.dp(15), kivy.metrics.dp(15), self.height//4), close=True, width=2)
+        self.circle1 = Line(circle=(kivy.metrics.dp(15), kivy.metrics.dp(15), kivy.metrics.dp(15)//2), close=True, width=2)
         self.canvas.add(self.circle1)
         #
         # self.circle2 = Line(circle=(self.width * 3.1, self.width * 1.2, self.width // 4), close=True, width=2)
